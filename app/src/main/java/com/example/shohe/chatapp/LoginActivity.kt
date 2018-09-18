f3ac60843730e205d71acd3ec4d41e60558cc1ba
Shohe Ohtani <canada.shohe@gmail.com>
2018-09-18 13:10:24 +0900

    + set on click action on sign up button

--- app/src/main/java/com/example/shohe/chatapp/LoginActivity.kt
+++ app/src/main/java/com/example/shohe/chatapp/LoginActivity.kt
@@ -2,11 +2,24 @@ package com.example.shohe.chatapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        this.initSignUpButton()
+    }
+
+
+    // button action
+    private fun initSignUpButton() {
+        signupButton.setOnClickListener {
+            // transition sign up activity
+            Log.d("initSignUpButton()", "transition sign up activity.")
+        }
     }
 }
