b06824983f9760da608a9c572c7d8e1486e8860f
Shohe Ohtani <canada.shohe@gmail.com>
2018-09-18 13:05:46 +0900

    + transition to login activity

--- app/src/main/java/com/example/shohe/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/shohe/chatapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.shohe.chatapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.transitionLoinActivity()
+    }
+
+
+    // transition activity
+    private fun transitionLoinActivity() {
+        val intent: Intent = Intent(this, LoginActivity::class.java)
+        this.startActivity(intent)
     }
 }
